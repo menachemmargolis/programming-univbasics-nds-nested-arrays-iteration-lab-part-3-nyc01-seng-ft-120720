1b07b2496b9afccb2622ca1ff50e8c8e29a9ba49
menachem m margolis <margolismendel5728@gmail.com>
2020-11-26 20:30:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     element_index = 0
     array_string=""
     while element_index <src[row_index].count do
-      if src[row_index][element_index] == string
+      if src[row_index][element_index].class == string
         new_array+= src[row_index][element_index]
       end
       element_index+=1
